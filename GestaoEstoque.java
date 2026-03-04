cee0af84ca14da705032074f954b70bffe4f656d
vrc8vip-sys <vrc8vip@gmail.com>
2026-03-03 23:33:15 -0300

    Add files via upload
    Inicial de código

---
+++ GestaoEstoque.java
@@ -0,0 +1,49 @@
+/*
+ * Autores: Vanilson da Rocha Conceição E Carlos Alberto
+ * Curso: Análise e Desenvolvimento de Sistemas
+ * Disciplina: Programação Orientada a Objetos
+ * Data: __/__/____
+ * Descrição: Programa para _____________________________
+ */
+
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestaoEstoque {
+    public static void  main (String [] args){
+
+
+        static Produto {
+            String nome;
+            double peso;
+            double volume;
+            String categoria;
+            int quantidade;
+        }
+
+        Produto (String nome,double peso,double volume,String categorias,int quantidade){
+            this.nome = nome;
+            this.peso = peso;
+            this.volume = volume;
+            this.categoria = categoria;
+            this.quantidade = 0 //começa em 0
+        }
+
+       // Mostra informações do produto
+        void mostrar() {
+            System.out.println("Produto: " + nome);
+            System.out.println("  Peso: " + peso + " kg");
+            System.out.println("  Volume: " + volume + " m³");
+            System.out.println("  Categoria: " + categoria);
+            System.out.println("  Estoque: " + quantidade + " unidades");
+        }
+
+
+
+
+
+    }
+
+}
